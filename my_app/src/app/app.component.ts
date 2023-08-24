fc25cb09d7c197cc11ecbd8d5fbac740f7e3748d
Kundan Paudyal <Kundan.Paudyal@rci.rogers.ca>
2023-08-24 19:33:59 -0400

    first running basic two way binding

--- my_app/src/app/app.component.ts
+++ my_app/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  
+
   title = 'my_app';
+  username = "Hello"
+  newuser = "Kundan"
+ 
+  lgbtnclick(event: any){
+    
+    alert(this.username)
+    console.log(this.username)
+    console.log(event.target.value)
+ }
+ onKey(event: any) {
+  // throw new Error('Method not implemented.');
+  console.log(event.target.value)
+  }
+
+  clearValue(){
+    this.newuser = ""
+  }
+
+
 }
